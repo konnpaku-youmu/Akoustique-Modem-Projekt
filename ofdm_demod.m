916c4644e704d6ed5f9efdb1a9edbc31e5137d2b
YZ <charliehe0211@gmail.com>
2021-12-06 21:57:55 +0100

    fixed channel estimation in Session 5

--- ofdm_demod.m
+++ ofdm_demod.m
@@ -1,5 +1,5 @@
-function [H_esti, demod_sequence] = ofdm_demod(ofdm_packet, frame_len, prefix_len, on_bit_indices, train_block)
-    frame_len_half = frame_len / 2;
+function [H_esti, demod_sequence] = ofdm_demod(ofdm_packet, train_block, lt, ld, frame_len, prefix_len, on_bit_indices)
+    frame_len_half = (frame_len-2) / 2;
     % serial to parallel
     ofdm_frames = reshape(ofdm_packet, frame_len + prefix_len, []);
     % clip the prefix
@@ -8,18 +8,18 @@ function [H_esti, demod_sequence] = ofdm_demod(ofdm_packet, frame_len, prefix_le
     % demodulate & channel equalization
     demod_frames = fft(ofdm_frames);
 
+    packetserial = reshape(demod_frames, [], 1);
+
     train_block_diag = diag([0;train_block;0;conj(flipud(train_block))]);
-    for sub_carr = 1:100
-        % prevent sigularity
-%         indices = [2:frame_len_half, frame_len_half+2:frame_len];
-        H_esti = lsqr(train_block_diag, demod_frames(:, sub_carr), 1e-5, 50);
-%         H_esti = train_block_diag \ demod_frames(:, sub_carr);
-        demod_frames(:, sub_carr) = demod_frames(:, sub_carr)./H_esti;
-    end
+    X = repmat(train_block_diag, [100, 1]);
+    H_esti = lsqr(X, packetserial, 1e-5, 100);
+    
+    % Channel Equalization
+    demod_frames = demod_frames./H_esti;
 
     % parallel to serial
     if(isempty(on_bit_indices))
-        demod_sequence = reshape(demod_frames(2:frame_len_half, :), [], 1);
+        demod_sequence = reshape(demod_frames(2:frame_len_half+1, :), [], 1);
     else
         demod_frames_upper = demod_frames(2:frame_len_half, :);
         demod_on_freq_bins = demod_frames_upper(on_bit_indices, :);
--- transmit_pic.m
+++ transmit_pic.m
@@ -1,10 +1,9 @@
-% Exercise session 4: DMT-OFDM transmission scheme
 clear;
 
 Nq = 6;
 SNR = 25;
 frame_length = 514;
-prefix_len = 800;
+prefix_len = 256;
 
 [bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
 qamStream = qam_mod(bitStream, 2^Nq, SNR);
@@ -13,11 +12,11 @@ train_bits = randi([0, 1], (frame_length/2 -1)*Nq, 1);
 train_block = qam_mod(train_bits, 2^Nq, SNR);
 
 % OFDM modulation
-txOfdmStream = ofdm_mod(qamStream, frame_length, prefix_len);
-% 
-% % Channel
-% rxOfdmStream = txOfdmStream;
-% 
+txOfdmStream = ofdm_mod(qamStream, train_block, 20, 50, frame_length, prefix_len, []);
+
+% Channel
+rxOfdmStream = simulate_channel(txOfdmStream, 20, 50, frame_length, prefix_len)';
+
 % % OFDM demodulation
 % rxQamStream = ofdm_demod(rxOfdmStream, frame_length, prefix_len);
 % 
